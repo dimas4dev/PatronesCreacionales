16eaa448614c9130fe642b301411e34667487c2b
Dimas Mendoza <dimas.mendoza@3tcapital.co>
2024-02-14 11:35:20 -0500

    Implementar patrón Singleton en ApiClient

--- Singleton/realExamplePatern.js
+++ Singleton/realExamplePatern.js
@@ -1,6 +1,25 @@
+/**
+ * Clase ApiClient que implementa el patrón de diseño Singleton.
+ * Este patrón asegura que solo exista una instancia de ApiClient en la aplicación,
+ * proporcionando un único punto de acceso global a esta instancia.
+ *
+ * @class
+ */
 class ApiClient {
+    /**
+     * La única instancia de ApiClient.
+     * @static
+     * @private
+     */
     static instance;
 
+    /**
+     * Constructor de la clase ApiClient.
+     * Si ya existe una instancia de ApiClient, devuelve esa instancia en lugar de crear una nueva.
+     * Esto impide la creación de múltiples instancias de la clase.
+     *
+     * @param {string} baseUrl - La URL base de la API a la cual este cliente se conectará.
+     */
     constructor(baseUrl) {
         if (ApiClient.instance) {
             return ApiClient.instance;
@@ -10,6 +29,13 @@ class ApiClient {
         ApiClient.instance = this;
     }
 
+    /**
+     * Realiza una petición GET a la API.
+     * 
+     * @async
+     * @param {string} path - El camino específico a ser agregado a la baseUrl para la petición.
+     * @returns {Promise<Object|null>} La respuesta de la API como un objeto o null en caso de error.
+     */
     async get(path) {
         try {
             const response = await fetch(this.baseUrl + path);
@@ -23,19 +49,22 @@ class ApiClient {
         }
     }
 
-    // Puedes agregar aquí más métodos para POST, PUT, DELETE, etc.
+    // Aquí podrías agregar más métodos para soportar otras operaciones HTTP como POST, PUT, DELETE, etc.
 }
 
-// Creando la instancia Singleton con la URL base de la API
+// Ejemplo de uso del patrón Singleton con ApiClient
+
+// Creando la instancia Singleton con la URL base de la API.
 const apiClient = new ApiClient('https://miapi.com/v1/');
 
-// En otro lugar del código, tratando de redeclarar la instancia para reflejar el singleton pattern y su valor
+// En otro lugar del código, tratando de redeclarar la instancia.
+// Esta línea no creará una nueva instancia; en su lugar, devolverá la instancia ya creada.
 const sameApiClient = new ApiClient('https://miapi.com/v2/');
+
+// Demostrando que ambas variables apuntan a la misma instancia.
+console.log(apiClient === sameApiClient); // true
+
+// Uso de la instancia para realizar una petición GET.
 sameApiClient.get('/ruta-de-recurso')
     .then(data => console.log(data))
     .catch(error => console.error(error));
-
-console.log(apiClient);
-console.log(sameApiClient); 
-console.log(apiClient === sameApiClient); // true, son la misma instancia
-
